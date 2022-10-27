ef5614f45d6d9212317ca8900c35bd288abaf43d
Ivan Silvestre <isilvestreneto@gmail.com>
2022-10-27 10:47:51 -0300

    return values and types

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -1,5 +1,11 @@
 const prefix = '🐉 ';
 
+type ProductType = {
+  id: number;
+  name: string;
+  icon?: string;
+};
+
 export default async function updateOutput(id: string) {
   // TODO
 }
@@ -19,7 +25,7 @@ function runTheLearningSamples() {
   console.log(`${prefix} function declaration`);
   console.log(addNumbersDeclaration(7, 11));
 
-  function addNumbersDeclaration(x: number, y: number) {
+  function addNumbersDeclaration(x: number, y: number): number {
     const sum: number = x + y;
     return sum;
   }
@@ -31,4 +37,36 @@ function runTheLearningSamples() {
 
   console.log(`${prefix} function expression`);
   console.log(addNumbersExpression(77, 111));
+
+  const sampleProducts = [
+    { id: 10, name: 'Pizza slice', icon: 'fas fa-pizza-slice' },
+    { id: 20, name: 'Ice cream', icon: 'fas fa-ice-cream' },
+    { id: 30, name: 'Cheese', icon: 'fas fa-cheese' },
+  ];
+
+  function getProductNames(): string[] {
+    return sampleProducts.map((p) => p.name);
+  }
+
+  console.log(`${prefix} return array`);
+  console.log(getProductNames());
+
+  function getProductById(id: number): ProductType | undefined {
+    return sampleProducts.find((p) => id === p.id);
+  }
+
+  console.log(`${prefix} return ProductType`);
+  console.log(getProductById(30));
+
+  function displayProducts(products: ProductType[]) {
+    const productNames = products.map((p) => {
+      const name = p.name.toLowerCase();
+      return name;
+    });
+    const msg = `Sample products include: ${productNames.join(', ')}`;
+    console.log(`${prefix} return void`);
+    console.log(msg);
+  }
+
+  displayProducts(sampleProducts);
 }
